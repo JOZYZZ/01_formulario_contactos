48e97abcc8b0ae1f1426a7574ef145e00f519e8c
Guillermo Josias Tobal <josiastobal16@gmail.com>
2026-02-02 22:36:39 -0300

    avanze 1 html

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo "Proyecto php funcionando"
+// Esto es PHP y se ejecuta en el servidor
+$mensaje = "Proyecto PHP funcionando correctamente";
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Proyecto 01 - Formulario de Contacto</title>
+</head>
+<body>
+
+    <h1><?php echo $mensaje; ?></h1>
+
+    <p>Este es el inicio de nuestro proyecto con PHP.</p>
+
+</body>
+</html>
